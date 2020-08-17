ae7e6178512ef96d5abfcb5102aa11d79404d6c0
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2020-08-17 15:23:13 -0300

    Typescript intro

--- types-typescript/dist/clases-herencia.js
+++ types-typescript/dist/clases-herencia.js
@@ -1,85 +1,66 @@
-export { };
-enum PhotoOrientation {
-    Landscape = 0,
-    Portrait = 1,
-    Squere = 2,
-    Panorama = 3
-}
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var PhotoOrientation;
+(function (PhotoOrientation) {
+    PhotoOrientation[PhotoOrientation["Landscape"] = 0] = "Landscape";
+    PhotoOrientation[PhotoOrientation["Portrait"] = 1] = "Portrait";
+    PhotoOrientation[PhotoOrientation["Squere"] = 2] = "Squere";
+    PhotoOrientation[PhotoOrientation["Panorama"] = 3] = "Panorama";
+})(PhotoOrientation || (PhotoOrientation = {}));
 //Creamos una nueva clase que defina las propiedad id, title y sean heredadas por Picture y Album
-class Item{
-    private _id: number;
-    private _title: string;
-
-    constructor(id: number, title: string){
+class Item {
+    constructor(id, title) {
         this._id = id;
         this._title = title;
     }
-
-    get id(){
+    get id() {
         return this._id;
     }
-
-    get title(){
+    get title() {
         return this._title;
     }
-
-    set id(id: number){
-        this._id = id;
-    }
-
-    set title(title: string){
+    set title(title) {
         this._title = title;
     }
-
-    public toString(){
+    toString() {
         return `[id: ${this._id}, title: ${this._title}]`;
     }
 }
-
 //Otra forma de aplicar propiedades privadas en anteponiendo el simbolo #. 
-class Picture extends Item{
-    
-    private _orientation: PhotoOrientation;
-
-    constructor(id: number, title: string, orientation: PhotoOrientation) {
+class Picture extends Item {
+    constructor(id, title, orientation) {
         super(id, title);
         this._orientation = orientation;
     }
-
     get orientation() {
         return this._orientation;
     }
-
-    set orientation(orientation: PhotoOrientation) {
+    set orientation(orientation) {
         this._orientation = orientation;
     }
-
     //Comportamiento
-    public toString() {
+    toString() {
         return `[id: ${super.id}, ${super.title}, ${this._orientation}]`;
     }
 }
-
+//Miembro accesible a nivel de clase
+Picture.photoOrientation = PhotoOrientation;
 class Album extends Item {
-    private pictures: Picture[];
-
-    constructor(id: number, title: string) {
+    constructor(id, title) {
         super(id, title);
         this.pictures = [];
     }
-
-    public addPicture(picture: Picture) {
+    addPicture(picture) {
         this.pictures.push(picture);
     }
 }
-
-const album: Album = new Album(1, 'Family pics');
+const album = new Album(1, 'Family pics');
 album.addPicture(new Picture(1, 'Stay at home', PhotoOrientation.Landscape));
 console.log('Album ', album);
-
 //Accediendo a los miembros publicos
 console.log(`Album id: ${album.id}`); //invocamos al metodo get id()
 //accedemos al metodo set title()
 album.title = 'Vaciones 2020';
 console.log(`Album Title: ${album.title}`);
+// Accediendo al miembro static de la clase Picture
+console.log(`PhotoOrientation: ${Picture.photoOrientation.Landscape}`);
--- types-typescript/src/clases-herencia.ts
+++ types-typescript/src/clases-herencia.ts
@@ -1,65 +1,87 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var PhotoOrientation;
-(function (PhotoOrientation) {
-    PhotoOrientation[PhotoOrientation["Landscape"] = 0] = "Landscape";
-    PhotoOrientation[PhotoOrientation["Portrait"] = 1] = "Portrait";
-    PhotoOrientation[PhotoOrientation["Squere"] = 2] = "Squere";
-    PhotoOrientation[PhotoOrientation["Panorama"] = 3] = "Panorama";
-})(PhotoOrientation || (PhotoOrientation = {}));
+export { };
+enum PhotoOrientation {
+    Landscape = 0,
+    Portrait = 1,
+    Squere = 2,
+    Panorama = 3
+}
+
 //Creamos una nueva clase que defina las propiedad id, title y sean heredadas por Picture y Album
-class Item {
-    constructor(id, title) {
+abstract class Item{
+    //Implementacion del modo readonly para el id
+    private readonly _id: number;
+    private _title: string;
+
+    constructor(id: number, title: string){
         this._id = id;
         this._title = title;
     }
-    get id() {
+
+    get id(){
         return this._id;
     }
-    get title() {
+
+    get title(){
         return this._title;
     }
-    set id(id) {
-        this._id = id;
-    }
-    set title(title) {
+
+    set title(title: string){
         this._title = title;
     }
-    toString() {
+
+    public toString(){
         return `[id: ${this._id}, title: ${this._title}]`;
     }
 }
+
 //Otra forma de aplicar propiedades privadas en anteponiendo el simbolo #. 
-class Picture extends Item {
-    constructor(id, title, orientation) {
+class Picture extends Item{
+    //Miembro accesible a nivel de clase
+    public static photoOrientation = PhotoOrientation;
+
+    private _orientation: PhotoOrientation;
+
+    constructor(id: number, title: string, orientation: PhotoOrientation) {
         super(id, title);
         this._orientation = orientation;
     }
+
     get orientation() {
         return this._orientation;
     }
-    set orientation(orientation) {
+
+    set orientation(orientation: PhotoOrientation) {
         this._orientation = orientation;
     }
+
     //Comportamiento
-    toString() {
+    public toString() {
         return `[id: ${super.id}, ${super.title}, ${this._orientation}]`;
     }
 }
+
 class Album extends Item {
-    constructor(id, title) {
+    private pictures: Picture[];
+
+    constructor(id: number, title: string) {
         super(id, title);
         this.pictures = [];
     }
-    addPicture(picture) {
+
+    public addPicture(picture: Picture) {
         this.pictures.push(picture);
     }
 }
-const album = new Album(1, 'Family pics');
+
+const album: Album = new Album(1, 'Family pics');
 album.addPicture(new Picture(1, 'Stay at home', PhotoOrientation.Landscape));
 console.log('Album ', album);
+
 //Accediendo a los miembros publicos
 console.log(`Album id: ${album.id}`); //invocamos al metodo get id()
 //accedemos al metodo set title()
 album.title = 'Vaciones 2020';
 console.log(`Album Title: ${album.title}`);
+
+// Accediendo al miembro static de la clase Picture
+console.log(`PhotoOrientation: ${Picture.photoOrientation.Landscape}`);
